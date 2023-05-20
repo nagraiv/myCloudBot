9e8b457ca9812d50989940aa06dcd680132da697
GraN <117268180+nagraiv@users.noreply.github.com>
2023-05-20 17:54:26 +0700

    start func

--- main.py
+++ main.py
@@ -4,6 +4,10 @@ import os
 
 bot = telebot.TeleBot(os.environ['TOKEN'])
 
+@bot.message_handler(commands=["start"])
+def start(message):
+    bot.send_message(message.chat.id, 'Вас приветствует эхо бот!')
+
 
 @bot.message_handler(content_types=["text"])
 def echo(message):
