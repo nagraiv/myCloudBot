93a7ba5db1883ca56eebd0cc1949b6c4c9c531bf
GraN <117268180+nagraiv@users.noreply.github.com>
2023-05-20 19:56:46 +0700

    help func

--- main.py
+++ main.py
@@ -5,6 +5,11 @@ import os
 bot = telebot.TeleBot(os.environ['TOKEN'])
 
 
+@bot.message_handler(commands=["help"])
+def help(message):
+    bot.send_message(message.chat.id, 'Я эхо бот. Я умею только отвечать в точности то, что вы мне написали.')
+
+
 @bot.message_handler(commands=["start"])
 def start(message):
     bot.send_message(message.chat.id, 'Вас приветствует эхо бот!')
