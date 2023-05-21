5735c7661fdb06b47bcc7460c19293e543b5c90c
GraN <117268180+nagraiv@users.noreply.github.com>
2023-05-21 23:03:14 +0700

    detailed calendar

--- main.py
+++ main.py
@@ -2,8 +2,32 @@ import telebot
 from telebot import types
 import os
 
+from telegram_bot_calendar import DetailedTelegramCalendar, LSTEP
+
 
 bot = telebot.TeleBot(os.environ['TOKEN'])
+
+
+@bot.message_handler(commands=['date'])
+def choose_date(m):
+    calendar, step = DetailedTelegramCalendar().build()
+    bot.send_message(m.chat.id,
+                     f"Select {LSTEP[step]}",
+                     reply_markup=calendar)
+
+
+@bot.callback_query_handler(func=DetailedTelegramCalendar.func())
+def cal(c):
+    result, key, step = DetailedTelegramCalendar().process(c.data)
+    if not result and key:
+        bot.edit_message_text(f"Select {LSTEP[step]}",
+                              c.message.chat.id,
+                              c.message.message_id,
+                              reply_markup=key)
+    elif result:
+        bot.edit_message_text(f"You selected {result}",
+                              c.message.chat.id,
+                              c.message.message_id)
 
 
 def keyboard():
